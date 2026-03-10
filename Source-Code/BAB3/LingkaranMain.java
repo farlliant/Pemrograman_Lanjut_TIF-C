25f45224e6aa8aa0e676544fb440e5426223a777
farlliant <farrellbrilliantt@gmail.com>
2026-03-10 20:39:20 +0700

    bab3

---
+++ Source-Code/BAB3/LingkaranMain.java
@@ -0,0 +1,9 @@
+public class LingkaranMain {
+    public static void main(String[] args) {
+        lingkaran l = new lingkaran(3);
+        l.setTinggi(10);
+        l.displayMessage();
+        lingkaran l2 = new lingkaran(4, 10);
+        l2.displayMessage();
+    }
+}
